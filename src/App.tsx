99010296a6ffca1de42d2189e7eee220b939ef9e
Stitch <97808356+stitcharg@users.noreply.github.com>
2023-10-23 16:23:38 -0300

    Reemplazo de GA por Clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { Box, Flex, Heading, Text, VStack } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import "react-datepicker/dist/react-datepicker.css";
 import { CalculatorComponent } from './components/Calculator'
 import { ExtraInfo } from './components/ExtraInfo'
 import { FooterComponent } from './components/Footer'
 import { HeaderComponent } from './components/Header'
-import ReactGA from 'react-ga';
+import { clarity } from 'react-microsoft-clarity';
 
 function App() {
 
-	if (import.meta.env.PROD && typeof window !== 'undefined') {
-		ReactGA.initialize('UA-221362845-1');
-		ReactGA.pageview(window.location.pathname + window.location.search);
-	}
+	//if (import.meta.env.PROD)
+	useEffect(() => {
+		clarity.init(
+			import.meta.env.VITE_CLARITY_ID
+		);
+	}, []);
 
 	return (
 		<div className="App">
